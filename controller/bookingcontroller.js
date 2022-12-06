32a9add021917a12ce3e337da42c99ec8f68ac62
Jinu Shaji <jinu.shaji@experionglobal.local>
2022-12-06 21:56:36 +0530

    updated

--- controller/bookingcontroller.js
+++ controller/bookingcontroller.js
@@ -4,23 +4,33 @@ const user = require('../model/user');
 const place = require('../model/place');
 const booking = require('../model/booking');
 // const Booking = require('../model/booking');
-const Place = require('../model/place');
 
 
-module.exports.create = (req, res, next) => {
-    place.findAll().then(places => {
-        res.render('booking-form', {
-            data: places
-        });
-    })
 
+module.exports.create = async (req, res, next) => {
+    const id = req.params.id;
+    const places = await place.findAll();
+    const cabFromDb = await cab.findByPk(id);
+    const userFromDb = await user.findByPk(req.session.userId);
+    console.log(cabFromDb.dataValues);
+    res.render('booking-form', {
+        id: id,
+        data: places,
+        cab: cabFromDb,
+        user: userFromDb
+    });
 }
 
 module.exports.createPost = (req, res, next) => {
+    console.log("hlo");
+
+    
+
     booking.create({
         bid: req.body.bid,
         pid: req.body.pid,
-        uid: req.body.uid,
+        cid:req.body.cid,
+        uid: req.session.userId,
         date: req.body.date,
         time: req.body.time,
         paddress: req.body.paddress,
@@ -28,14 +38,36 @@ module.exports.createPost = (req, res, next) => {
         pname: req.body.pname,
         contactnumber: req.body.contactnumber,
         passengersno: req.body.passengersno,
-
     })
         .then(user => {
-            res.redirect("/cab");
+            res.redirect("/cab/bookview/"+user.bid);
         })
 }
 
 
+module.exports.bookview = (req, res, next) => {
+    console.log(req.params.bid);
+    
+    booking.findByPk(req.params.bid)
+        .then(bookings => {
+            console.log(bookings);
+            res.render('bookview', {
+                data: bookings
+
+
+            })
+        });
+
+}
+module.exports.payment = (req, res, next) => {
+    res.render('payment');
+}
+module.exports.paymentsuccess = (req, res, next) => {
+    res.render('successpdf');
+}
+module.exports.pdf = (req, res, next) => {
+    res.render('pdf');
+}
 
 
 
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,7 +4,12 @@ const controller = require('../controller/bookingController');
 const router = express.Router();
 
 router.get('/booking/add/:id', controller.create);
-router.post('/booking/add', controller.createPost);
+router.post('/booking/add/:id', controller.createPost);
+router.get('/cab/bookview/:bid', controller.bookview);
+router.get('/cab/payment', controller.payment);
+router.get('/cab/payment/success', controller.paymentsuccess);
+router.get('/cab/payment/successpdf', controller.pdf);
+
 
 
 
